b2c304ccd4c4c1b1d61b3cfa2d26fac8ed302418
DennisOnder <webdev.dennisonder@gmail.com>
2019-03-10 00:05:27 +0100

    Array

---
+++ basics/arrays.php
@@ -0,0 +1,16 @@
+<?php
+    // Indexed array
+    $people = array('Kevin', 'Dennis', 'Jeremy');
+    $ids = array(101,123,155);
+    $cars = ['Ford', 'Volkswagen', 'Honda'];
+    array_push($cars, 'BMW');
+    count($cars);
+
+    // Associative arrays
+    $newArray = array('Brad' => 35, 'Jose' => 32, 'William' => 25);
+    echo $newArray['Brad'];
+
+    // Debugging array functions
+    // var_dump();
+    // print_r();
+?>
